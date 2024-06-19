da343cb8765f374eb7f48eee93f83316c1555625
kumarnavlesh604 <kumarnavlesh604@gmail.com>
2024-06-19 17:03:42 +0000

    Arithmeticoperator.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+//  Arithmetic operator
 let x = 50;
 let y = 30;
 console.log(x+y)
@@ -7,3 +7,14 @@ console.log(x*y)
 console.log(x/y)
 console.log(x%y)
 console.log(x**y)
+
+
+
+
+// Assigment operator
+
+let a=10;
+a +=5
+console.log(a)
+a/=2;
+console.log(a)
